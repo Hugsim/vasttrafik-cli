6f07e2dec6336f2a343a1a6b93f6f2ec8d4c8e83
Hugo Simonsson <simonssonhugo13@gmail.com>
2023-03-09 16:39:51 +0100

    Added two commands: Search for stops and Plan for planning trips

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+use std::error::Error;
+
+use api::endpoints::{location_name::*, trip::call_trip};
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+    #[clap(subcommand)]
+    subcommand: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    Search {
+        #[clap(short, long)]
+        station: String,
+        #[clap(short, long)]
+        count: Option<usize>,
+    },
+    Plan {
+        #[clap(short, long)]
+        from: String,
+        #[clap(short, long)]
+        to: String,
+    },
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let mut token = api::get_token()?;
+
+    match args.subcommand {
+        SubCommand::Search { station, count } => {
+            let places = call_location_name(&mut token, &station)?
+                .location_list
+                .stop_location;
+            let count = match count {
+                Some(count) => count,
+                None => places.len(),
+            };
+
+            for p in &places[..count] {
+                println!("{}", p.name);
+            }
+        }
+        SubCommand::Plan { from, to } => {
+            let from_id = &&call_location_name(&mut token, &from)?
+                .location_list
+                .stop_location[0]
+                .id;
+            let to_id = &&call_location_name(&mut token, &to)?
+                .location_list
+                .stop_location[0]
+                .id;
+            let res = call_trip(&mut token, from_id, to_id)?;
+            println!("{:?}", res);
+        }
+    }
+
+    Ok(())
 }
